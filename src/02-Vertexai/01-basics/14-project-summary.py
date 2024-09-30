4eac105dac383b43ca52baf24523dad05fb4fdf2
bhagavan <bhagavan@auranetworks.in>
2024-09-30 16:29:42 +0530

    Updating files

---
+++ src/02-Vertexai/01-basics/14-project-summary.py
@@ -0,0 +1,98 @@
+from vertexai.generative_models import Part
+from vertexai.generative_models import GenerativeModel
+
+def process_pdf_number_of_tokens():
+    file_uri = "gs://bhagavan-bucket/genai-resources/beginners-must-do.pdf"
+    
+    pdf_file = Part.from_uri(file_uri, mime_type="application/pdf")
+    
+    prompt = "How many tokens can the model process?"
+    
+    model = "gemini-1.5-pro-001"
+    contents = [pdf_file, prompt]
+    
+    multimodal_model = GenerativeModel(model)
+
+    response = multimodal_model.generate_content(contents=contents)
+    print(response.text)
+
+def process_pdf_summarize():
+    file_uri = "gs://bhagavan-bucket/genai-resources/beginners-must-do.pdf"
+    pdf_file = Part.from_uri(file_uri, mime_type="application/pdf")
+    
+    prompt = """
+    You are a professional document summarization specialist
+    Please summrize the given document
+    """
+    
+    model = "gemini-1.5-pro-001"
+    contents = [pdf_file, prompt]
+    
+    multimodal_model = GenerativeModel(model)
+
+    response = multimodal_model.generate_content(contents=contents)
+    print(response.text)
+
+def process_pdf_solution():
+    file_uri = "gs://bhagavan-bucket/genai-resources/beginners-must-do.pdf"
+    pdf_file = Part.from_uri(file_uri, mime_type="application/pdf")
+    
+    prompt = """
+    You are a professional document summarization specialist
+    How many section are available?
+    """
+    
+    model = "gemini-1.5-pro-001"
+    contents = [pdf_file, prompt]
+    
+    multimodal_model = GenerativeModel(model)
+
+    response = multimodal_model.generate_content(contents=contents)
+    print(response.text)
+    
+def process_pdf_titles():
+    file_uri = "gs://bhagavan-bucket/genai-resources/beginners-must-do.pdf"
+    pdf_file = Part.from_uri(file_uri, mime_type="application/pdf")
+    
+    prompt = """
+    You are a professional document summarization specialist
+    Can you suggest better titles for each section?
+    """
+    
+    model = "gemini-1.5-pro-001"
+    contents = [pdf_file, prompt]
+    
+    multimodal_model = GenerativeModel(model)
+
+    response = multimodal_model.generate_content(contents=contents)
+    print(response.text)
+
+def summarize_pdf_document():
+    # https://storage.googleapis.com/bhagavan-pub-bucket/project.pdf
+    
+    file_uri = "gs://bhagavan-pub-bucket/project.pdf"
+    pdf_file = Part.from_uri(file_uri, mime_type="application/pdf")
+    
+    prompt = """
+    You are a professional document summarization specialist
+    Can you summarise the list of activities involved?
+    Make sure each activity can be implimented idipedentely
+    Integration can be done at later stage
+    """
+    
+    model = "gemini-1.5-pro-001"
+    contents = [pdf_file, prompt]
+    
+    multimodal_model = GenerativeModel(model)
+
+    response = multimodal_model.generate_content(contents=contents)
+    print(response.text)
+
+    
+def main():
+    # process_pdf_titles()
+    summarize_pdf_document()
+    
+if __name__ == "__main__":
+    main()
+    
--- src/02-Vertexai/02-rag/04-genCode-NoRAG.py
+++ src/02-Vertexai/02-rag/04-genCode-NoRAG.py
@@ -3,7 +3,7 @@ from langchain_google_vertexai import VertexAI
 def main():
     code_llm = VertexAI(model_name="gemini-1.5-pro", max_output_tokens=2048, temperature=0.1, verbose=False)
 
-    user_question = "Create a Python function with debug statement using pdbwhereami moduel to print Line number, functio name, and call stack at appropriate places in the function"
+    user_question = "Create a Python function with debug statements using pdbwhereami module to print Line number, function name, and call stack at appropriate places in the function"
 
     response = code_llm.invoke(input=user_question, max_output_tokens=2048, temperature=0.1)
     print(response)
